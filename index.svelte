3efb35e00797557e515152c272227d3b40f6953e
dealdreygaroeb@gmail.com <dealdreygaroeb@gmail.com>
2022-08-18 23:50:27 +0200

    update ui

--- index.svelte
+++ index.svelte
@@ -1,3 +1,3 @@
-<bar .underline><icon>chevron-left</icon> <text>Spectre demo</text><icon load=xtra.svelte>plus</icon></bar>
-<button @on:click=love .outline><icon >plus</icon>add love</button>
+<bar .underline.matte><icon>chevron-left</icon> <text>Spectre demo</text><icon load=xtra.svelte>plus</icon></bar>
+<button @on:click=love .outline>add some <icon>heart-fill</icon></button>
 
